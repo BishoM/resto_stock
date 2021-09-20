7bd7e2a7b37487d5803f1f47b7a5f96e93a018f4
BISHOBOKERUWIZEYE Moise <77962774+Bishobokeruwizeye@users.noreply.github.com>
2021-09-20 12:02:58 -0700

    main branch

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Restaurant Stock</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+   
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+
+<header>
+   
+    <div id="nav-head" class="header-nav">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-md-3 no-padding col-sm-12 nav-img">
+                    <img src="assets/images/logo.png" style="width:80px;margin-top: 0%;" alt="">
+                    <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                </div>
+                <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
+                    <ul>
+                        <li><a href= class="homered">HOME</a></li>
+                        <li><a href="" class="homeblack">STOCKIN</a></li>
+                        <li><a href="" class="homeblack">DELVERED</a></li>
+                        <li><a href="" class="homeblack">STOCKOUT</a></li>
+                        <li><a href="index.html" class="homeblack">logout</a></li> 
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</header>
